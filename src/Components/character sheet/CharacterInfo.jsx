23dbc3116f109df84fd0d77dde42d2aed1482361
JordanDyck <jordandyck777@hotmail.com>
2024-11-06 12:32:20 -0700

    removed duplicates from class features

--- src/Components/character sheet/CharacterInfo.jsx
+++ src/Components/character sheet/CharacterInfo.jsx
@@ -7,31 +7,32 @@ const CharacterInfo = () => {
   const character = useSelector((store) => store.character.value)
   const [url, setUrl] = useState()
   const levelList = character.levels.slice(0, character.currentLevel)
-  let featureNameBlackList = [
-    "Spellcasting: ",
-    "Ability Score Improvement",
-    " (d",
-  ]
+  let featureNameBlackList = ["Spellcasting: ", "Ability Score Improvement"]
   const getCurrentLevelFeatures = () => {
     // pulls all features up to current level & stores them in an array.
     let featureList = []
     levelList.map((lvl) => {
       return lvl.features.map((feature) => {
         if (!featureNameBlackList.some((el) => feature.name.includes(el))) {
-          return featureList.push(feature)
+          return (featureList = [
+            ...featureList,
+            {
+              index: feature.index,
+              name: feature.name.split(" (").shift(), // (for duplicates) removes the () to only show the name
+              url: feature.url,
+            },
+          ])
         } else return ""
       })
     })
-    return featureList
+    return featureList.filter(
+      (ele, index) =>
+        index === featureList.findIndex((elem) => elem.name === ele.name)
+    )
   }
 
   const displayLevelFeatures = () => {
-    //removes duplicate features
-    const uniqueFeatureList = [
-      ...new Map(getCurrentLevelFeatures().map((el) => [el.name, el])).values(),
-    ]
-
-    return uniqueFeatureList.map((feature) => {
+    return getCurrentLevelFeatures().map((feature) => {
       return (
         <p key={feature.index} onClick={() => setUrl({name: feature.url})}>
           {feature.name}
